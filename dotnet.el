4b21a79a9b57aad6286877cf3a497d12d09a598b
Sebastián Monía <sebastian.monia@starz.com>
2019-07-23 15:18:24 -0600

    Re use last project/solution unless command is called with prefix arg

--- dotnet.el
+++ dotnet.el
@@ -38,35 +38,101 @@
   :group 'dotnet
   :type 'string)
 
+(defcustom dotnet-mode-verbosity-level "normal"
+  "Verbosity level used when invoking the dotnet executable.
+Check the documentation of your version of dotnet for valid values. \"diag\" is useful for troubleshooting."
+  :group 'dotnet
+  :type 'string)
+
+(defvar dotnet-langs '("c#" "f#") "Languages supported by this package.")
+(defvar dotnet-templates '("console" "classlib" "mstest" "xunit" "web" "mvc" "webapi") "List of dotnet project templates supported by this package.")
+;; Using "" as a default instead of nil.  Then we can call string-suffix-p without ceremony.
+(defvar dotnet-current-target "" "The solution/project on which the next command will operate.")
+(defvar dotnet-command-log-buffer-name "*dotnet commands*" "History of dotnet commands executed")
+
+(defvar dotnet-test-last-test-proj nil
+  "Last unit test project file executed by `dotnet-test'.")
+
+(defun dotnet--log-command (cmd-string)
+  "Append CMD-STRING to the log buffer."
+  (let ((log-buffer (get-buffer-create dotnet-command-log-buffer-name)))
+    (with-current-buffer log-buffer
+      (goto-char (point-max))
+      (insert cmd-string)
+      (insert "\n"))))
+
+(defun dotnet--current-target-or-prompt ()
+  "Return the current target or prompt for one.  With prefix arg prompt anyway."
+  (when (or current-prefix-arg
+            (string= dotnet-current-target ""))
+    (setq dotnet-current-target (dotnet--select-project-or-solution)))
+  dotnet-current-target)
+
+(defun dotnet--current-target-or-prompt-sln-only ()
+  "Return the current target or prompt for one, only if it's a solution.
+With prefix arg prompt anyway."
+  (dotnet--keep-target-if-solution)
+  (when (or current-prefix-arg
+            (string= dotnet-current-target ""))
+    (setq dotnet-current-target (dotnet--select-project-or-solution t)))
+  dotnet-current-target)
+
+(defun dotnet--keep-target-if-project ()
+  "Keep current target only if it is a project."
+  (unless (string-suffix-p ".csproj" dotnet-current-target)
+    (setq dotnet-current-target "")))
+
+(defun dotnet--keep-target-if-solution ()
+  "Keep current target only if it is a solution."
+p  (unless (string-suffix-p ".sln" dotnet-current-target)
+    (setq dotnet-current-target "")))
+
+(defun dotnet--select-project-or-solution (&optional sln-only)
+  "Prompt for the project/solution file.  Try projectile root first, else use current buffer's directory.
+When SLN-ONLY allow only solutions."
+  (let ((default-dir-prompt default-directory)
+        (filter-regex (if sln-only
+                    "\\.sln$"
+                  "\\.csproj$\\|\\.sln$")))
+    (ignore-errors
+      (when (and (fboundp 'projectile-project-root)
+                 (projectile-project-root))
+        (setq default-dir-prompt (projectile-project-root))))
+    (expand-file-name (completing-read
+                       "Project or solution: "
+                       (directory-files-recursively default-dir-prompt filter-regex)))))
+
+(defun dotnet--verbosity-param ()
+  "Return the verbosity parameter to append where applicable."
+  (format "-v %s" dotnet-mode-verbosity-level))
+
 ;;;###autoload
 (defun dotnet-add-package (package-name)
   "Add package reference from PACKAGE-NAME."
   (interactive "sPackage name: ")
-  (dotnet-command (concat "dotnet add package " package-name)))
+  (dotnet--keep-target-if-project)
+  (dotnet-targeted-command (concat "dotnet add %s package " (shell-quote-argument package-name))))
 
 ;;;###autoload
-(defun dotnet-add-reference (reference project)
-  "Add a REFERENCE to a PROJECT."
-  (interactive (list (read-file-name "Reference file: ")
-                     (read-file-name "In Project: ")))
-  (dotnet-command (concat "dotnet add " project " reference "  reference)))
+(defun dotnet-add-project-reference (reference)
+  "Add a REFERENCE to a project."
+  (interactive (list (read-file-name "Reference file: ")))
+  (dotnet--keep-target-if-project)
+  (dotnet-targeted-command (concat "dotnet add %s reference "  (shell-quote-argument reference))))
 
 ;;;###autoload
 (defun dotnet-build ()
   "Build a .NET project."
   (interactive)
-  (let* ((target (dotnet-select-project-or-solution))
-         (command "dotnet build -v n \"%s\""))
+  (let* ((target (dotnet--select-project-or-solution))
+         (command (concat "dotnet build " (dotnet--verbosity-param)  " %s")))
     (compile (format command target))))
 
 ;;;###autoload
 (defun dotnet-clean ()
   "Clean build output."
   (interactive)
-  (dotnet-command "dotnet clean -v n"))
-
-(defvar dotnet-langs '("c#" "f#"))
-(defvar dotnet-templates '("console" "classlib" "mstest" "xunit" "web" "mvc" "webapi"))
+  (dotnet-targeted-command (format "dotnet clean %s %%s" (dotnet--verbosity-param))))
 
 ;;;###autoload
 (defun dotnet-new (project-path template lang)
@@ -86,25 +152,20 @@ language (see `dotnet-langs')."
 (defun dotnet-publish ()
   "Publish a .NET project for deployment."
   (interactive)
-  (dotnet-command "dotnet publish -v n"))
+  (dotnet-targeted-command (format "dotnet publish %s %%s " (dotnet--verbosity-param))))
 
 ;;;###autoload
 (defun dotnet-restore ()
   "Restore dependencies specified in the .NET project."
   (interactive)
-  (dotnet-command "dotnet restore"))
-
-(defvar dotnet-run-last-proj-dir nil
-  "Last project directory executed by `dotnet-run'.")
+  (dotnet-targeted-command (format "dotnet restore %s %%s " (dotnet--verbosity-param))))
 
 ;;;###autoload
-(defun dotnet-run (arg)
-  "Compile and execute a .NET project.  With ARG, query for project path again."
-  (interactive "P")
-  (when (or (not dotnet-run-last-proj-dir) arg)
-    (setq dotnet-run-last-proj-dir (read-directory-name "Run project in directory: ")))
-  (let ((default-directory dotnet-run-last-proj-dir))
-    (dotnet-command (concat "dotnet run " dotnet-run-last-proj-dir))))
+(defun dotnet-run ()
+  "Compile and execute a .NET project."
+  (interactive)
+  (dotnet--keep-target-if-project)
+  (dotnet-targeted-command (format "dotnet run  %s --project %%s " (dotnet--verbosity-param))))
 
 ;;;###autoload
 (defun dotnet-run-with-args (args)
@@ -116,24 +177,24 @@ language (see `dotnet-langs')."
 (defun dotnet-sln-add ()
   "Add a project to a Solution."
   (interactive)
-  (let ((solution-file (read-file-name "Solution file: ")))
-    (let ((to-add (read-file-name "Project/Pattern to add to the solution: ")))
-      (dotnet-command (concat "dotnet sln " solution-file " add " to-add)))))
+  (let ((solution-file (dotnet--current-target-or-prompt-sln-only))
+        (to-add (read-file-name "Project/Pattern to add to the solution: ")))
+    (dotnet-command (concat "dotnet sln " (shell-quote-argument solution-file) " add " to-add))))
 
 ;;;###autoload
 (defun dotnet-sln-remove ()
   "Remove a project from a Solution."
   (interactive)
-  (let ((solution-file (read-file-name "Solution file: ")))
-    (let ((to-remove (read-file-name "Project/Pattern to remove from the solution: ")))
-      (dotnet-command (concat "dotnet sln " solution-file " remove " to-remove)))))
+  (let ((solution-file (dotnet--current-target-or-prompt-sln-only))
+        (to-remove (read-file-name "Project/Pattern to remove from the solution: ")))
+    (dotnet-command (concat "dotnet sln " (shell-quote-argument solution-file) " remove " to-remove))))
 
 ;;;###autoload
 (defun dotnet-sln-list ()
   "List all projects in a Solution."
   (interactive)
-  (let ((solution-file (read-file-name "Solution file: ")))
-    (dotnet-command (concat "dotnet sln " solution-file " list"))))
+  (let ((solution-file (dotnet--current-target-or-prompt-sln-only)))
+    (dotnet-command (concat "dotnet sln " (shell-quote-argument solution-file) " list"))))
 
 ;;;###autoload
 (defun dotnet-sln-new ()
@@ -142,78 +203,28 @@ language (see `dotnet-langs')."
   (let ((solution-path (read-directory-name "Solution path: ")))
     (dotnet-command (concat "dotnet new sln -o " solution-path))))
 
-(defvar dotnet-test-last-test-proj nil
-  "Last unit test project file executed by `dotnet-test'.")
-
 ;;;###autoload
 (defun dotnet-test (arg)
   "Launch project unit-tests, querying for a project on first call.  With ARG, query for project path again."
   (interactive "P")
   (when (or (not dotnet-test-last-test-proj) arg)
-    (setq dotnet-test-last-test-proj (read-file-name "Launch tests for Project file: ")))
-  (dotnet-command (concat "dotnet test " dotnet-test-last-test-proj)))
+    (setq dotnet-test-last-test-proj (dotnet--select-project-or-solution)))
+  (dotnet-command (format "dotnet test %s %s" (dotnet--verbosity-param) (shell-quote-argument dotnet-test-last-test-proj))))
 
 (defun dotnet-command (cmd)
   "Run CMD in an async buffer."
+  (dotnet--log-command (concat "dotnet-command @ " default-directory))
+  (dotnet--log-command cmd)
   (async-shell-command cmd "*dotnet*"))
 
-(defun dotnet-find (extension)
-  "Search for a EXTENSION file in any enclosing folders relative to current directory."
-  (dotnet-search-upwards (rx-to-string extension)
-                         (file-name-directory buffer-file-name)))
-
-(defun dotnet-goto (extension)
-  "Open file with EXTENSION in any enclosing folders relative to current directory."
-  (let ((file (dotnet-find extension)))
-    (if file
-        (find-file file)
-      (error "Could not find any %s file" extension))))
-
-(defun dotnet-goto-sln ()
-  "Search for a solution file in any enclosing folders relative to current directory."
-  (interactive)
-  (dotnet-goto ".sln"))
-
-(defun dotnet-goto-csproj ()
-  "Search for a C# project file in any enclosing folders relative to current directory."
-  (interactive)
-  (dotnet-goto ".csproj"))
-
-(defun dotnet-goto-fsproj ()
-  "Search for a F# project file in any enclosing folders relative to current directory."
-  (interactive)
-  (dotnet-goto ".fsproj"))
-
-(defun dotnet-search-upwards (regex dir)
-  "Search for REGEX in DIR."
-  (when dir
-    (or (car-safe (directory-files dir 'full regex))
-        (dotnet-search-upwards regex (dotnet-parent-dir dir)))))
-
-(defun dotnet-parent-dir (dir)
-  "Find parent DIR."
-  (let ((p (file-name-directory (directory-file-name dir))))
-    (unless (equal p dir)
-      p)))
-
-(defun dotnet-select-project-or-solution ()
-  "Prompt for the project/solution file or directory.  Try projectile root first, else use current buffer's directory."
-  (let ((default-dir-prompt "?"))
-    (ignore-errors
-      (when (fboundp 'projectile-project-root)
-        (setq default-dir-prompt (projectile-project-root))))
-    (when (string= default-dir-prompt "?")
-      (setq default-dir-prompt default-directory))
-    (expand-file-name (read-file-name "Project or solution: " default-dir-prompt nil t))))
-
-(defun dotnet-valid-project-solutions (path)
-  "Predicate to validate project/solution paths.  PATH is passed by `'read-file-name`."
-  ;; file-attributes returns t for directories
-  ;; if not a dir, then check the common extensions
-  (let ((extension (file-name-extension path))
-        (valid-projects (list "sln" "csproj" "fsproj")))
-    (or (member extension valid-projects)
-        (car (file-attributes path)))))
+(defun dotnet-targeted-command (cmd-to-format)
+  "Run CMD-TO-FORMAT in an async buffer, with a project/solution target replaced in."
+  (let* ((target (dotnet--current-target-or-prompt))
+         (default-directory (file-name-directory target))
+         (cmd-string (format cmd-to-format (shell-quote-argument target))))
+    (dotnet--log-command (concat "dotnet-targeted-command @ " default-directory))
+    (dotnet--log-command cmd-string)
+    (async-shell-command cmd-string "*dotnet*")))
 
 (defvar dotnet-mode-command-map
   (let ((map (make-sparse-keymap)))
@@ -221,9 +232,6 @@ language (see `dotnet-langs')."
     (define-key map (kbd "a r") #'dotnet-add-reference)
     (define-key map (kbd "b")   #'dotnet-build)
     (define-key map (kbd "c")   #'dotnet-clean)
-    (define-key map (kbd "g c") #'dotnet-goto-csproj)
-    (define-key map (kbd "g f") #'dotnet-goto-fsproj)
-    (define-key map (kbd "g s") #'dotnet-goto-sln)
     (define-key map (kbd "n")   #'dotnet-new)
     (define-key map (kbd "p")   #'dotnet-publish)
     (define-key map (kbd "r")   #'dotnet-restore)
